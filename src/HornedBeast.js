68bb8fd961a330700ae5bef214082acbd03fc84e
Don Choi <dchoi294@gmail.com>
2022-11-08 23:40:40 +0000

    progress of lab 2

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,10 +1,25 @@
 import React from 'react';
 
 class HornedBeast extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      favorites : 0
+    };
+  }
+
+  handleFavorites = () => {
+    this.state({
+      favorites: this.state.favorites + 1
+    });
+  }
+
   render() {
     return (
       <article>
         <h2>{this.props.name}</h2>
+        <p>{this.state.favorites} favorites</p>
+        <p onClick={this.handleFavorites}> :D</p>
         <img 
           src={this.props.imageURL}
           alt={this.props.description}

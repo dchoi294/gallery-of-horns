875b95fb1b5c42e13fada5b0e6bd09703546e05e
Don Choi <dchoi294@gmail.com>
2022-11-09 21:46:19 +0000

    final lab1

--- src/Main.js
+++ src/Main.js
@@ -4,19 +4,19 @@ import HornedBeast from './HornedBeast';
 class Main extends React.Component {
   render() {
     return (
-      <main>
+      <article>
+        <HornedBeast 
+          title="unicorn"
+          imageURL ="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"
+          description = "Unicorn!!!!!"
+        />  
         <HornedBeast
-        title='narwhal'
-        imageUrl='http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg'
-        description='narwhal!'
+          title="Rhino"
+          imageURL ="https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80"
+          description = "Rhino!!!"
         />
-        <HornedBeast
-        title='rhino'
-        imageUrl='https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80'
-        description='Rhino!'
-        />
-      </main>
-    )
+      </article>
+    );
   }
 }
 
